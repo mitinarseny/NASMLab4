303323810584e27d4ae74b09b98a15818994f515
Arseny Mitin <mitinarseny@gmail.com>
2020-03-30 13:33:55 +0300

    feat: fill B and C (reverse) arrays

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@
 build: lab4
 
 lab4: main.c
-	gcc --std=c11 -Wall -O3 -o $@ -g $^
+	gcc --std=c11 -Wall -O0 -o $@ -g $^
--- main.c
+++ main.c
@@ -16,7 +16,7 @@ void c(int8_t A[16]) {
         count++;
     }
 
-    printf("SUM: %d\n\t\tB: [", sm);
+    printf("COUNT: %d\n\t\tSUM: %d\n\t\tB: [", count, sm);
     for (int i = 0; i < count; i++)
         printf("%4d ", B[i]);
     printf("]\n\t\tC: [");
@@ -28,38 +28,46 @@ void c(int8_t A[16]) {
 void f(int8_t A[16]) {
     int16_t sm = 0;
     int8_t count = 0;
-    int8_t B[16];
-    int8_t C[16];
-
+    int8_t B[16] = {0};
+    int8_t C[16] = {0};
+   
     __asm__ (
-        "xor %[sm], %[sm]\n\t"
-        "mov $16, %%ecx\n"
-        "1:\n\t"
-        "mov (%[A]), %%al\n\t"
-        "cmp $-8, %%al\n\t"
-        "jle 2f\n\t"
-        "cbw\n\t"
-        "mov %%ax, %%dx\n\t"
-        "sar $31, %%dx\n\t"
-        "xor %%dx, %%ax\n\t"
-        "sub %%dx, %%ax\n\t"
-        "add %%ax, %[sm]\n\t"
+        "xor %[sm], %[sm]\n\t"       // sum <- 0
+        "xor %[count], %[count]\n" // count <- 0
+        "1:\n\t"                     // loop: iterate over A
+        "mov (%[A]), %%al\n\t"       // AL <- A[i]
+        "cmp $-8, %%al\n\t"          // if (A[i] <= -8)
+        "jle 2f\n\t"                 // continue
+        "mov %%al, (%[B])\n\t"
+        "mov %%cl, (%[C])\n\t"
+        "cbw\n\t"                    // AL -> AX
+        "mov %%ax, %%dx\n\t"         // DX <- AX               -|
+        "sar $31, %%dx\n\t"          // DX <- sign bit of A[i]  |
+        "xor %%dx, %%ax\n\t"         // AX <- AX xor DX         |--> abs(A[i])
+        "sub %%dx, %%ax\n\t"         // AX <- DX - AX          -|
+        "add %%ax, %[sm]\n\t"        // sum += abs(A[i])
+        "inc %[count]\n\t"             // count += 1
+        "inc %[B]\n\t"
+        "inc %[C]\n"
         "2:\n\t"
-        "inc %[A]\n"
-        "loop 1b"
-    : [sm] "=b" (sm)
-    : [A] "S" (A),
-    [B] "D" (B)
-    : "ecx", "ax", "dx"
+        "inc %[A]\n\t"              // i += 1
+        "loop 1b\n"                    //   goto 1
+    : [sm] "=b" (sm),
+    [count] "=r" (count)
+    : [size] "c" (16),
+    [A] "S" (A),
+    [B] "D" (B),
+    [C] "r" (C)
+    : "ax", "dx"
     );
     
-    printf("SUM: %d\n\t\tB: [", sm);
+    printf("COUNT: %d\n\t\tSUM: %d\n\t\tB: [", count, sm);
     for (int i = 0; i < count; i++)
         printf("%4d ", B[i]);
     printf("]\n\t\tC: [");
     for (int i = 0; i < count; i++)
         printf("%4d ", C[i]);
-    printf("]\n");
+    printf("]\n\t\t");
 }
 
 int main() {
